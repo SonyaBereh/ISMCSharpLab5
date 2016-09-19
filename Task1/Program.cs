dc058c3ea5a8094c3223bc2339a298dcb31db9cd
SonyaBereh <chernovskaya.sophia@yandex.ua>
2016-09-19 18:22:20 +0300

    Практическая работа №5

---
+++ Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите х: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите y: ");
+            double y = double.Parse(Console.ReadLine());
+
+
+            double z, q;
+            z = (x + (2 + y) / (x * x)) / (y + 1 / (Math.Sqrt(x * x + 10)));
+            q = 2.8 * Math.Sin(x) + Math.Abs(y);
+            if ((y + 1 / (Math.Sqrt(x * x + 10))) == 0)
+            {
+            Console.WriteLine("q = "+ q +".");
+            }
+            else
+            Console.WriteLine("z = "+ z +".");
+            Console.WriteLine("q = " + q + ".");
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите а: ");
+            int a = int.Parse(Console.ReadLine());
+            int a4, a6, a7, a8, a9, a10, a13, a15, a21, a28, a64;
+
+            a4 = a * a;
+            a4 *= a4;
+
+            a6 = a4;
+            a6 = a6 * a6 / a / a;           
+            
+            a7 = a6 * a;
+
+            a8 = a4;
+            a8 *= a8;
+
+            a9 = a8 * a;
+
+            a10 = a9 * a;
+
+            a13 = a10 * a4 / a;
+
+            a15 = a8;
+            a15 = a15 * a15 / a;
+
+            a21 = a6;
+            a21 = a21 * a21 * a21 / a / a / a;
+
+            a28 = a7;
+            a28 = a28 * a28 * a28;
+
+            a64 = a21;
+            a64 = a64 * a64;
+      
+            
+
+
+
+
+        }
+    }
+}
