764af144630287218fa6b77439d92c6fc4427b8e
SonyaBereh <chernovskaya.sophia@yandex.ua>
2016-09-21 13:59:57 +0300

    Практическая работа №5

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -12,38 +12,92 @@ namespace Task2
         {
             Console.WriteLine("Введите а: ");
             int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите N: ");
+            int N = int.Parse(Console.ReadLine());
             int a4, a6, a7, a8, a9, a10, a13, a15, a21, a28, a64;
-
-            int a2 = a * a;
-            a4 = a2;
+            switch(N)
+            {  
+                case 4:
+            a4 = a*a;
             a4 *= a4;
+            Console.WriteLine(a4);
+                    break;
+                case 6:
+            int b = a*a;
+            int c = b*b;
+                    a6 = c*b;
+            Console.WriteLine(a6);
+                    break;
+                case 7:
+            b = a*a;
+            c = b*b;
+            a6 = c*b;
+            a7 = a6*a;
+            Console.WriteLine(a7);
+                    break;
+                case 8:
+            b = a*a;
+            c = b*b;
+            a8 = c*c;
+            Console.WriteLine(a8);
+                    break;
+                case 9:
+           b = a*a;
+           c = b*b;
+           int d = c*c;
+           a9 = d * a;
+            Console.WriteLine(a9);
+                    break;
+                case 10:
+            b = a * a;
+            c = b * b;
+            d = c * c;
+            a10 = d * b;
+            Console.WriteLine(a10);
+                    break;
+                case 13:
+            b = a*a;
+            c = b*b*b;
+            a13 = c*c*a;
+            Console.WriteLine(a13);
+                    break;
+                case 15:
+            b = a * a;
+            c = b * b;
+            d = c * c;
+            a15 = d * d * (1 / a);
+            Console.WriteLine(a15);
+                    break;
+                case 21:
+           b = a*a;
+           c = b*b;
+           d = c*c;
+           a21 = d*d*c*a;
+           Console.WriteLine(a21);
+                    break;
 
-            a6 = a2;
-            a6 *= a4;
-                        
-            a7 = a6 * a;
+                case 28:
+            b = a*a;
+            c = b*b;
+            d = c*b;
+            int e = d*a;
+            a28 = e*e*e;            
+            Console.WriteLine(a28);
+            break;
 
-            a8 = a4;
-            a8 *= a8;
-
-            a9 = a8 * a;
-
-            a10 = a4;
-            a10 *= a10
-
-           
-
-           
-
-            a28 = a7;
-            a28 = a28 * a28 * a28;
-
-            a64 = a4;
+                case 64:
+            a64 = a*a;
             a64 *= a64;
             a64 *= a64;
             a64 *= a64;
             a64 *= a64;
-      
+            a64 *= a64;
+            Console.WriteLine(a64);
+                    break;
+
+                default : Console.WriteLine("Error");
+                    break;
+            }
             
 
 
